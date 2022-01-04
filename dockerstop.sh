ff07f230a1ce83495f204d591dbf092de041d733
vsonka <vikassonkar07@gmail.com>
2022-01-04 22:34:31 +0000

    script

--- dockerstop.sh
+++ dockerstop.sh
@@ -1,1 +1,1 @@
-sudo docker ps -a | grep vikson36/dockerpoc |  awk '{print $1}'  | xargs docker stop
+sudo docker ps -a | grep vikson36/dockerpoc |  awk '{print $1}'  | xargs docker stop > /dev/null 2>&1 echo 'stopped container' || echo 'nothing to stop'
